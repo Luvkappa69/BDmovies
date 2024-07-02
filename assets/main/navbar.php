9ad370bee90ecdb68dd42e5120d6624d3fc13fae
Luvkappa69 <diogo.silva7a@gmail.com>
2024-07-02 01:07:14 +0100

    colors update

--- assets/main/navbar.php
+++ assets/main/navbar.php
@@ -4,13 +4,19 @@
             return "activemenu";
         }
     }
+    //nav contents:
+    $labels = ['Cinemas', 'Filmes', 'Sessões'];
+    $mainFiles = ['cinemas.php', 'filmes.php', 'sessoes.php'];
+    $listFiles = ['listaCinemas.php', 'listaFilmes.php', 'listaSessoes.php'];
+
+
     if(isset($_SESSION['utilizador']) && $_SESSION['tipo'] == 1){
 ?>
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-            <img src="src/img/logo.png">
+            <img src="src/img/logo.png" id="logo">
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -19,44 +25,27 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-
-
-                <li class="nav-item dropdown <?= isActive('cinemas.php') ?> <?= isActive('listaCinemas.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Cinemas
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('cinemas.php') ?>" href="cinemas.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaCinemas.php') ?>" href="listaCinemas.php">Listar</a></li>
-                    </ul>
-                </li>
-
-                <li class="nav-item dropdown <?= isActive('filmes.php') ?> <?= isActive('listaFilmes.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Filmes
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('filmes.php') ?>" href="filmes.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaFilmes.php') ?>" href="listaFilmes.php">Listar</a></li>
-                    </ul>
-                </li>
-
-                <li class="nav-item dropdown <?= isActive('sessoes.php') ?> <?= isActive('listaSessoes.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Sessões
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('sessoes.php') ?>" href="sessoes.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaSessoes.php') ?>" href="listaSessoes.php">Listar</a></li>
-                    </ul>
-                </li>
-                
-
+                <?php
+                foreach ($labels as $index => $label) {
+                    $mainFile = $mainFiles[$index];
+                    $listFile = $listFiles[$index];
+                    ?>
+                    <li class="nav-item dropdown <?= isActive($mainFile) ?> <?= isActive($listFile) ?>">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= $label ?>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item <?= isActive($mainFile) ?>" href="<?= $mainFile ?>">Adicionar</a></li>
+                            <li><a class="dropdown-item <?= isActive($listFile) ?>" href="<?= $listFile ?>">Listar</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
                 <li class="nav-item <?= isActive('main.php') ?>">
                     <a class="nav-link" href="main.php" role="button" aria-expanded="false">Profile</a>
                 </li>
             </ul>
         </div>
+
     </div>
 </nav>
 
@@ -74,37 +63,24 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-            <li class="nav-item dropdown <?= isActive('cinemas.php') ?> <?= isActive('listaCinemas.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Cinemas
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('cinemas.php') ?>" href="cinemas.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaCinemas.php') ?>" href="listaCinemas.php">Listar</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown <?= isActive('cinemas.php') ?> <?= isActive('listaCinemas.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Filmes
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('filmes.php') ?>" href="filmes.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaFilmes.php') ?>" href="listaFilmes.php">Listar</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown <?= isActive('sessoes.php') ?> <?= isActive('listaSessoes.php') ?>">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Sessões
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item <?= isActive('sessoes.php') ?>" href="sessoes.php">Adicionar</a></li>
-                        <li><a class="dropdown-item <?= isActive('listaSessoes.php') ?>" href="listaSessoes.php">Listar</a></li>
-                    </ul>
-                </li>
+                <?php
+                foreach ($labels as $index => $label) {
+                    $mainFile = $mainFiles[$index];
+                    $listFile = $listFiles[$index];
+                    ?>
+                    <li class="nav-item dropdown <?= isActive($mainFile) ?> <?= isActive($listFile) ?>">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= $label ?>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item <?= isActive($mainFile) ?>" href="<?= $mainFile ?>">Adicionar</a></li>
+                            <li><a class="dropdown-item <?= isActive($listFile) ?>" href="<?= $listFile ?>">Listar</a></li>
+                        </ul>
+                    </li>
+                <?php } ?>
                 <li class="nav-item <?= isActive('main.php') ?>">
                     <a class="nav-link" href="main.php" role="button" aria-expanded="false">Profile</a>
                 </li>
-                
             </ul>
         </div>
     </div>
